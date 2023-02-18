057b2d005330149fcfcdd47feaeee29e939ac213
Dimion <ardgalen@dimlev.ru>
2023-02-18 14:59:01 +0300

    19 and 23 tasks

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число: ");
+double Number = Convert.ToInt32(Console.ReadLine());
+
+
+for (double i = 1; i <= Number; i++)
+{
+   
+    double i2 = Math.Pow(i, 3);
+    System.Console.Write("\t" + i2);
+  
+}
